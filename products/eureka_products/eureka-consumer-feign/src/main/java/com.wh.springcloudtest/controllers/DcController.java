df19141959f47f3322db23a82bb39193a6697020
WangHong <ajax010@163.com>
2019-03-21 10:19:20 +0800

    服务消费Feign

---
+++ products/eureka_products/eureka-consumer-feign/src/main/java/com.wh.springcloudtest/controllers/DcController.java
@@ -0,0 +1,26 @@
+package com.wh.springcloudtest.controllers;
+
+import com.wh.springcloudtest.interfaces.DcClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @author WangHong
+ * @version 1.0.0
+ * @ClassName DcController.java
+ * @Description 服务消费（Feign）实例测试
+ * @date 2019/3/20 13:45
+ */
+@RestController
+public class DcController {
+
+    @Resource
+    private DcClient dcClient;
+
+    @GetMapping("/consumer")
+    public String dc() {
+        return dcClient.consumer();
+    }
+}
